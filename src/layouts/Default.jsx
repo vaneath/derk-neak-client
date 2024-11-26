e94be532419481f30395aaecbab33218b834a676
vaneath <vaneathzz@gmail.com>
2024-11-26 15:54:45 +0700

    Add default background layout

--- src/layouts/Default.jsx
+++ src/layouts/Default.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import BottomNav from "../components/BottomNav";
+import backgroundImage from "../assets/maps.svg";
 
 const Default = () => {
   return (
     <>
-      <BottomNav />
-      <Outlet />
+      <div
+        style={{
+          minHeight: "100vh",
+          minWidth: "100vw",
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "fit",
+          backgroundPosition: "top",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className='mx-auto max-w-[375px] w-full'>
+          <Outlet />
+        </div>
+        <BottomNav />
+      </div>
     </>
   );
 };
