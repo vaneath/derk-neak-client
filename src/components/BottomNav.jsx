f14c2d94bc5be16902481160a0f756a06c468d37
vaneath <vaneathzz@gmail.com>
2024-11-29 14:58:41 +0700

    Add top margin for bottom navigation

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -9,30 +9,32 @@ import { NavLink } from "react-router-dom";
 
 const BottomNav = () => {
   return (
-    <div className='fixed bottom-0 z-50 w-full h-16 bg-white border-t border-gray-200'>
-      <div className='relative flex justify-between sm:justify-evenly items-center mx-auto font-medium h-full px-4'>
-        {/* Left Side Icons */}
-        <div className='flex justify-start sm:justify-evenly gap-8 grow'>
-          <NavItem to='/' icon={GoHome} label='Home' />
-          <NavItem to='/tickets' icon={HiOutlineTicket} label='My Ticket' />
-        </div>
-
-        {/* Center Icon (Buy) */}
-        <div className='absolute -top-7 left-1/2 transform -translate-x-1/2'>
-          <div className='bg-primary size-14 rounded-full flex justify-center items-center shadow-md'>
-            <NavLink to='/search' className='text-white'>
-              <FaSearchLocation className='text-2xl' />
-            </NavLink>
+    <div className='mt-32'>
+      <div className='fixed bottom-0 z-50 w-full h-16 bg-white border-t border-gray-200'>
+        <div className='relative flex justify-between sm:justify-evenly items-center mx-auto font-medium h-full px-4'>
+          {/* Left Side Icons */}
+          <div className='flex justify-start sm:justify-evenly gap-8 grow'>
+            <NavItem to='/' icon={GoHome} label='Home' />
+            <NavItem to='/tickets' icon={HiOutlineTicket} label='My Ticket' />
           </div>
-          <span className='text-center text-xs text-gray-500 mt-1 block'>
-            Buy
-          </span>
-        </div>
 
-        {/* Right Side Icons */}
-        <div className='flex justify-end sm:justify-evenly gap-8 grow'>
-          <NavItem to='/promotions' icon={CiDiscount1} label='Promo' />
-          <NavItem to='/account' icon={IoPersonOutline} label='Account' />
+          {/* Center Icon (Buy) */}
+          <div className='absolute -top-7 left-1/2 transform -translate-x-1/2'>
+            <div className='bg-primary size-14 rounded-full flex justify-center items-center shadow-md'>
+              <NavLink to='/search' className='text-white'>
+                <FaSearchLocation className='text-2xl' />
+              </NavLink>
+            </div>
+            <span className='text-center text-xs text-gray-500 mt-1 block'>
+              Buy
+            </span>
+          </div>
+
+          {/* Right Side Icons */}
+          <div className='flex justify-end sm:justify-evenly gap-8 grow'>
+            <NavItem to='/promotions' icon={CiDiscount1} label='Promo' />
+            <NavItem to='/account' icon={IoPersonOutline} label='Account' />
+          </div>
         </div>
       </div>
     </div>
--- src/components/NavigationButton.jsx
+++ src/components/NavigationButton.jsx
@@ -3,14 +3,16 @@ import { NavLink } from "react-router-dom";
 
 const NavigationButton = ({ text, to }) => {
   return (
-    <div className='fixed bottom-0 left-0 w-full bg-white rounded-t-xl shadow-[0_-4px_6px_rgba(0,0,0,0.1)]'>
-      <div className='p-4'>
-        <NavLink
-          to={to}
-          className='w-full block py-2 h-10 bg-primary text-white text-center font-medium rounded-md hover:opacity-90 hover:text-white'
-        >
-          {text}
-        </NavLink>
+    <div className='mt-32'>
+      <div className='fixed bottom-0 left-0 w-full bg-white rounded-t-xl shadow-[0_-4px_6px_rgba(0,0,0,0.1)]'>
+        <div className='p-4'>
+          <NavLink
+            to={to}
+            className='w-full block py-2 h-10 bg-primary text-white text-center font-medium rounded-md hover:opacity-90 hover:text-white'
+          >
+            {text}
+          </NavLink>
+        </div>
       </div>
     </div>
   );
